5df564c997baba0d71f0d0c511e2157e07e6d0d3
Emmanuel TAIWO <emmanueltaiwo@Emmanuels-MacBook-Pro.local>
2023-12-10 16:06:18 +0100

    fix: Convert sign-up page to a functional component

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="w-full h-screen flex justify-center">
+      <form className="w-[700px] h-[600px] mt-7 rounded-[10px] shadow-sm shadow-gray-500 bg-[rgb(11,11,41)] flex justify-center"></form>
+    </div>
+  );
+};
+
+export default Signup;
