90c641fc0c818e64094da557a93fd7b8fdac6e20
Emmanuel Taiwo <devemmanuel1@gmail.com>
2023-12-28 17:48:28 +0100

    chore: integrated data from database to display in sidebar

---
+++ types/communities-types.ts
@@ -0,0 +1,9 @@
+export interface Community {
+  communityID: string;
+  communityName: string;
+  communityPic: string;
+  communityCategory: string;
+  isAdmim: boolean;
+  members: number;
+  communityMembers: string[];
+}
