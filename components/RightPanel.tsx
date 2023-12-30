63ff89b806fed3543811426d8a44db8688d86ac2
Emmanuel Taiwo <devemmanuel1@gmail.com>
2023-12-30 08:40:37 +0100

    chore: integrated communities  data from database to display in feed and integrated a recommendation algorithm for communities

--- components/RightPanel.tsx
+++ components/RightPanel.tsx
@@ -1,8 +1,19 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { recommendCommunityToUser } from "@/services/communities";
+import { Community } from "@/types/communities-types";
+import { useQuery } from "@tanstack/react-query";
 
 const RightPanel = () => {
+  const { data, isLoading } = useQuery<Community[]>({
+    queryKey: ["communities"],
+    queryFn: async () => await recommendCommunityToUser(),
+    staleTime: 0,
+  });
+
   return (
     <section className="hidden lg:inline lg:w-[25%] xl:w-[27%] bg-gray-300 dark:bg-gray-900 top-0 bottom-0 fixed right-0 overflow-y-auto transition-all duration-500">
       <div className="flex flex-col gap-5">
@@ -37,28 +48,43 @@ const RightPanel = () => {
             Recommended for you
           </h1>
 
-          <Link
-            href="/"
-            className="w-full flex items-center gap-2 mx-auto transition-all duration-300 hover:bg-[rgba(0,0,0,0.18)] p-2 rounded-[15px]"
-          >
-            <Image
-              src="/assets/Images/cover.png"
-              width={100}
-              height={100}
-              alt="community image"
-              className="rounded-full w-[40px] h-[40px]"
-            />
-            <div className="flex flex-col gap-1">
-              <h4 className="font-bold text-[rgb(26,32,44)] dark:text-[rgb(205,211,226)] text-[15px] text-center">
-                Clutch Maintainers
-              </h4>
-              <div className="flex gap-1 flex-wrap items-center">
-                <p className="text-[13px] font-[200] text-gray-700 dark:text-gray-300 italic">
-                  Technology
-                </p>
-              </div>
-            </div>
-          </Link>
+          {isLoading && (
+            <p className="font-bold text-[rgb(26,32,44)] dark:text-[rgb(205,211,226)] text-[15px] text-center">
+              Loading...
+            </p>
+          )}
+
+          {data && data.length < 1 ? (
+            <h3 className="text-[14px] font-[400] text-gray-900 dark:text-gray-200 flex items-center gap-5 w[90%] px-4 py-3 rounded-[15px] transition-all duration-200 bg-[rgba(125,133,150,0.86)] dark:bg-[rgba(38,47,66,0.86)]">
+              No community was found
+            </h3>
+          ) : (
+            data?.slice(0, 10).map((community) => (
+              <Link
+                key={community.communityID}
+                href={`communities/${community.communityCategory}/${community.communityID}`}
+                className="w-full flex items-center gap-2 mx-auto transition-all duration-300 hover:bg-[rgba(0,0,0,0.18)] p-2 rounded-[15px]"
+              >
+                <Image
+                  src={community.communityPic ?? "/assets/Images/cover.png"}
+                  width={100}
+                  height={100}
+                  alt="community image"
+                  className="rounded-full w-[40px] h-[40px]"
+                />
+                <div className="flex flex-col gap-1">
+                  <h4 className="font-bold text-[rgb(26,32,44)] dark:text-[rgb(205,211,226)] text-[15px] text-center">
+                    {community.communityName}
+                  </h4>
+                  <div className="flex gap-1 flex-wrap items-center">
+                    <p className="text-[13px] font-[200] text-gray-700 dark:text-gray-300 italic">
+                      {community.communityCategory}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            ))
+          )}
         </div>
 
         <div className="absolute bottom-0 left-0 right-0 w-[90%] flex flex-wrap gap-3 mx-auto py-5">
