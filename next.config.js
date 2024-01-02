d330c92138493447010fdd489c3ba4f5009eb8f5
Emmanuel Taiwo <devemmanuel1@gmail.com>
2024-01-02 20:13:53 +0100

    refactor: refactored next.config file to remotepatterns instead of domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
   },
 };
 
