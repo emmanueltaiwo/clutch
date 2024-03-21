f71cf18e34ab937e468ca90d56b1e4646186ec33
Emmanuel Taiwo <devemmanuel1@gmail.com>
2024-03-21 16:30:23 +0100

    chore: integrated react query for comments

--- components/post-details/CommentContainer.tsx
+++ components/post-details/CommentContainer.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { useState, useEffect, useMemo } from "react";
-import { Comment, User } from "@/types";
-import { collection, query, onSnapshot } from "firebase/firestore";
-import { db } from "@/firebase";
-import { getUserDocFromFirestore } from "@/services/auth";
-import { formatDate } from "@/utils/helpers";
+import { Comment } from "@/types";
 import CommentCard from "./CommentCard";
 import CommentSkeleton from "./CommentSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import { fetchPostComments } from "@/services/feed";
 
 const CommentContainer = ({
   defaultUserId,
@@ -16,68 +14,45 @@ const CommentContainer = ({
   defaultUserId: string;
   postId: string;
 }) => {
-  const [comments, setComments] = useState<Comment[]>([]);
+  const {
+    data: comments,
+    isLoading,
+    isError,
+  } = useQuery<Comment[]>({
+    queryKey: ["post-comment", postId],
+    queryFn: async () => await fetchPostComments(postId),
+    staleTime: 0,
+  });
   const [showNoCommentsMessage, setShowNoCommentsMessage] = useState(false);
 
   useEffect(() => {
-    const fetchAllComments = async () => {
-      try {
-        const q = query(collection(db, "comments"));
-
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-          const promises = querySnapshot.docs
-            .filter((doc) => doc.data().postId === postId)
-            .map(async (doc) => {
-              const comment = doc.data() as Comment;
-              const user = (await getUserDocFromFirestore(
-                comment.userId
-              )) as User;
-
-              return {
-                commentId: comment.commentId,
-                userId: comment.userId,
-                postId: comment.postId,
-                commentText: comment.commentText,
-                createdAt: comment.createdAt,
-                updatedAt: comment.updatedAt,
-                createdAtString: formatDate(comment.createdAt),
-                updatedAtString: formatDate(comment.updatedAt),
-                user: {
-                  fullName: user.fullName,
-                  profilePic: user.profilePic,
-                  country: user.country,
-                },
-              };
-            });
-
-          Promise.all(promises).then((newComments) => {
-            setComments(newComments);
-            
-              setShowNoCommentsMessage(newComments.length === 0);
-            
-          });
-        });
-
-        return () => unsubscribe();
-      } catch (error) {
-        throw new Error();
-      }
-    };
-
-    fetchAllComments();
-  }, [postId]);
+    setShowNoCommentsMessage(comments?.length === 0);
+  }, [comments]);
 
   const sortedComments = useMemo(() => {
-    return [...comments].sort((a, b) => {
-      return b.createdAt - a.createdAt;
-    });
+    return (
+      comments &&
+      [...comments].sort((a, b) => {
+        return b.createdAt - a.createdAt;
+      })
+    );
   }, [comments]);
 
   const skeletonCards = Array.from({ length: 5 }, (_, index) => (
-    <div key={index} className="w-[95%] mx-auto h-full flex flex-col gap-3">
+    <div key={index} className="w-[95%] mx-auto flex flex-col gap-8 mt-5">
       <CommentSkeleton />
     </div>
   ));
+
+  if (isError) {
+    return (
+      <div>
+        <p className="text-white font-bold text-[40px] text-center mt-20">
+          An error occurred
+        </p>
+      </div>
+    );
+  }
 
   return (
     <section className="w-full flex flex-col pb-20">
@@ -85,10 +60,10 @@ const CommentContainer = ({
         <div className="text-center py-5 text-[14px] text-gray-800 dark:text-gray-600">
           Be the first to comment
         </div>
-      ) : sortedComments.length < 1 ? (
+      ) : isLoading ? (
         skeletonCards
       ) : (
-        sortedComments.map((comment) => (
+        sortedComments?.map((comment) => (
           <CommentCard
             defaultUserId={defaultUserId}
             key={comment.commentId}
