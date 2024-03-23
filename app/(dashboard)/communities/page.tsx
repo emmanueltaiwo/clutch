7f86d6f8169747306758ff01d149ab16a293f25d
Emmanuel Taiwo <devemmanuel1@gmail.com>
2024-03-23 19:03:54 +0100

    [chore]: built community page

--- app/(dashboard)/communities/page.tsx
+++ app/(dashboard)/communities/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Container from "@/components/Container";
+import RightPanel from "@/components/RightPanel";
+import CommunityContainer from "@/components/communities/CommunityContainer";
 
 const CommunitiesPage = () => {
-  return <div>page</div>;
+  return (
+    <section className="flex justify-between">
+      <Container>
+        <h2 className="font-bold text-[25px] cursor-pointer p-5 mt-5">
+          Communities
+        </h2>
+        <CommunityContainer />
+      </Container>
+      <RightPanel />
+    </section>
+  );
 };
 
 export default CommunitiesPage;
---
+++ components/communities/CommunityContainer.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const CommunityContainer = () => {
+  return <section className="p-5">CommunityContainer</section>;
+};
+
+export default CommunityContainer;
